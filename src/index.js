0bd5bd87c1481b45957995182820e8ade0678f29
ruzbellit.romero <ruzbellit.romero@correounivalle.edu.co>
2023-01-27 20:48:06 -0500

    Reto codigo graphql

--- src/index.js
+++ src/index.js
@@ -10,12 +10,27 @@ const typeDefs = gql`
 
   type Query {
     Getbooks: [Book],
-    Getbook(id:String!):[Book]
+    Getbook(id:String!): Book
   }
   type Mutation {
       CreateBook(id: String!,title: String!, author: String!): Book
       DeleteBook(id: String!): Book
       UpdateBook(id: String!,title: String!, author: String!): Book 
+  }
+
+  type Quote {
+    id: String  
+    quote: String
+    author: String
+  }
+  type Query {
+    Getquotes: [Quote],
+    Getquote(id:String!): Quote
+  }
+  type Mutation {
+    CreateQuote(id: String!,quote: String!, author: String!): Quote
+    DeleteQuote(id: String!): Quote
+    UpdateQuote(id: String!,quote: String!, author: String!): Quote 
   }
 `;
 
@@ -36,6 +51,60 @@ let books = [
        author: 'Gabriel garcia Marquez',
     }
   ];
+
+  let breakingquotes = [
+    {
+      "quote": "Free food always tastes good. Free drinks even better.",
+      "author": "Hank Schrader",
+      "id": "0"
+    },
+    {
+      "quote": "Some people are immune to good advice.",
+      "author": "Walter White",
+      "id": "1"
+    },
+    {
+      "quote": "Better call Saul!",
+      "author": "Saul Goodman",
+      "id": "2"
+    },
+    {
+      "quote": "If you believe that there’s a hell, we’re pretty much already going there.",
+      "author": "Walter White",
+      "id": "3"
+    },
+    {
+      "quote": "Bitch!",
+      "author": "Jesse Pinkman",
+      "id": "4"
+    },
+    {
+      "quote": "Alright, tell you what. Both of you pull it out your butts right now, or I go grab a flashlight and some pliers and go exploring.",
+      "author": "Jesse Pinkman",
+      "id": "5"
+    },
+    {
+      "quote": "If I ever get anal polyps, I'll know what to name them.",
+      "author": "Saul Goodman",
+      "id": "6"
+    },
+    {
+      "quote": "Stop acting like such a baby.",
+      "author": "Walter White",
+      "id": "7"
+    },
+    {
+      "quote": "Sitting around, smoking marijuana, eating Cheetos and masturbating do not constitute \"plans\".",
+      "author": "Walter White",
+      "id": "8"
+    },
+    {
+      "quote": "Sometimes the forbidden fruit tastes the sweetest.",
+      "author": "Hank Schrader",
+      "id": "9"
+    }
+  ];
+
   const resolvers = {
     Mutation: {
         CreateBook: (_,arg) => {books.push(arg); return arg},
@@ -49,12 +118,30 @@ let books = [
                                  books[objIdx] = arg
                                  return arg   
              
-                              }                        
+                              },
+        CreateQuote: (_, arg) => {
+          breakingquotes.push(arg);
+          return arg
+        },
+        DeleteQuote: (_, arg) => {
+          let final = breakingquotes.filter(q => q.id != arg.id);
+          let deleted = breakingquotes.find(q => q.id == arg.id);
+          breakingquotes = [...final];
+          return deleted
+        },
+        UpdateQuote: (_, arg) => {
+          let objIdx = breakingquotes.findIndex(q => q.id == arg.id);
+          breakingquotes[objIdx] = arg
+          return arg
+        }                      
 
     },  
     Query: {
       Getbooks: () => books,
-      Getbook: (_,arg) => books.find(number => number.id==arg.id)
+      Getbook: (_,arg) => books.find(number => number.id==arg.id),
+
+      Getquotes: () => breakingquotes,
+      Getquote: (_, arg) => breakingquotes.find(q => q.id == arg.id)
     },
   };
 
